a87b6b5a87833f73fdd6c7e57486b8c4c7ab4128
Leland Taylor <letaylor@users.noreply.github.com>
2020-03-31 14:34:05 +0100

    cleaned data for plots

--- pipelines/0025-qc_normalization/bin/0057-scanpy_cluster_markers.py
+++ pipelines/0025-qc_normalization/bin/0057-scanpy_cluster_markers.py
@@ -7,9 +7,99 @@ __version__ = '0.0.1'
 
 import argparse
 import os
+import numpy as np
 import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import scanpy as sc
 import csv
+
+
+def nd(arr):
+    """Make nd array."""
+    return np.asarray(arr).reshape(-1)
+
+
+def dotplot(adata, markers):
+    """Customize dotplot function."""
+    adata_raw = adata.raw.to_adata()
+    cluter_ids = np.unique(
+        adata_raw.obs.cluster.values.astype(int)
+    ).astype(str)
+
+    features = adata_raw.var.gene_symbols.values
+    midx = [np.where(i == features)[0][0] for i in markers]
+
+    # for each cluster for each gene get two things
+    # 1 percent of cells in the cluster expressing that gene
+    # 2 average expression of that gene (for cells that are expressing it)
+    per = np.zeros((len(cluter_ids), len(markers)))
+    avg = np.zeros((len(cluter_ids), len(markers)))
+    mtx = adata_raw.X  # .todense()
+
+    for cn, c in enumerate(cluter_ids):
+        tmp_mtx = mtx[adata_raw.obs.cluster.values == c]
+        sub_mtx = tmp_mtx[:, midx]
+        avg[cn] = nd(sub_mtx.mean(axis=0))
+        per[cn] = (sub_mtx > 0).sum(axis=0) / sub_mtx.shape[0]
+
+    fig, ax = plt.subplots(
+        figsize=(len(markers) * 0.35, len(cluter_ids) * 0.3 + 1)
+    )
+    xidx = np.arange(len(markers))
+    yidx = np.arange(len(cluter_ids))
+
+    xlabels = markers
+    ylabels = cluter_ids
+
+    X, Y = np.meshgrid(xidx, yidx)
+
+    for dn, d in enumerate(per):
+        # a = ax.scatter(X[dn], Y[dn], s=d*500+10, c=avg[dn], cmap='Blues')
+        a = ax.scatter(X[dn], Y[dn], s=d*100, c=avg[dn], cmap='Blues')
+
+    ax.set_xticks(xidx)
+    ax.set_yticks(yidx)
+
+    ax.set_xticklabels(xlabels, rotation=90, ha='center')
+    ax.set_yticklabels(ylabels)
+
+    ax.set_xlabel('Gene')
+    ax.set_ylabel('Cluster')
+    ax.figure.colorbar(a, ax=ax, label='$ln(CPM+1)$')
+
+    handles = [
+        mpl.lines.Line2D([
+            0], [0], marker='o', color='w', label='  0%',
+            markerfacecolor='black', markersize=7
+        ),
+        mpl.lines.Line2D(
+            [0], [0], marker='o', color='w', label=' 25%',
+            markerfacecolor='black', markersize=10
+        ),
+        mpl.lines.Line2D(
+            [0], [0], marker='o', color='w', label=' 50%',
+            markerfacecolor='black', markersize=12
+        ),
+        mpl.lines.Line2D(
+            [0], [0], marker='o', color='w', label=' 75%',
+            markerfacecolor='black', markersize=13.5
+        ),
+        mpl.lines.Line2D(
+            [0], [0], marker='o', color='w', label='100%',
+            markerfacecolor='black', markersize=17
+        )
+    ]
+    ax.legend(
+        handles=handles,
+        loc='center left',
+        bbox_to_anchor=(1.15, 0.5),
+        title='Percent of cells'
+    )
+
+    # plt.show()
+
+    return(fig, ax)
 
 
 def main():
@@ -87,8 +177,15 @@ def main():
     # Load the AnnData file.
     adata = sc.read_h5ad(filename=options.h5)
 
-    # TODO: Check what data is being used here: raw, lognorm, or norm+scaled?
-    #
+    print(
+        'WARNING: All functions in this script set use_raw=True,\
+        assuming that adata.raw.to_adata stores ln(CPM+1) normalized data.'
+    )
+
+    # NOTE: You should be using the ln(CPM+1) data here. By default these
+    #       functions use the .raw attribute of AnnData if present which is
+    #       assumed to be ln(CPM+1).
+
     # Identify cell type makers.
     if options.rgm == 'wilcoxon':
         # If considering simple Wilcoxon vs t-test, choose Wilcoxon as
@@ -101,6 +198,7 @@ def main():
             groupby='cluster',
             groups='all',
             reference='rest',
+            use_raw=True,
             method='wilcoxon',
             n_genes=100,
             corr_method='bonferroni'
@@ -115,6 +213,7 @@ def main():
             groupby='cluster',
             groups='all',
             reference='rest',
+            use_raw=True,
             method='logreg',
             n_genes=100
         )
@@ -175,10 +274,9 @@ def main():
         compression='gzip'
     )
 
-    # TODO: Check what data is being used here: raw, lognorm, or norm+scaled?
     # Plot cell type makers.
     # Annoyingly, prefix hardcoded as rank_genes_groups_<cluster_id>.
-    sc.pl.rank_genes_groups(
+    _ = sc.pl.rank_genes_groups(
         adata,
         gene_symbols='gene_symbols',
         n_genes=25,
@@ -202,18 +300,58 @@ def main():
     # Make dataframe of the top 5 markers per cluster
     marker_df_plt = marker_df.groupby('cluster').head(3)
 
-    # TODO: Check what data is being used here: raw, lognorm, or norm+scaled?
+    # NOTE: You should be using the ln(CPM+1) data here. "$ln(CPM+1)$"
     # Plot cell type markers in dotplot.
-    # Annoyingly, prefix hardcoded as dotplot.
-    sc.pl.dotplot(
+    fig, ax = dotplot(
+        adata,
+        markers=marker_df_plt['gene_symbols'].values
+    )
+    fig.savefig(
+        'dotplot-{}.pdf'.format(out_file_base),
+        dpi=300,
+        bbox_inches='tight'
+    )
+    # Annoyingly, the prefix is hardcoded as dotplot. Should save to axes
+    _ = sc.pl.dotplot(
         adata,
         marker_df_plt['ensembl_gene_id'].to_list(),
         groupby='cluster',
         dendrogram=True,
-        # gene_symbols='gene_symbols',
+        # gene_symbols='gene_symbols',  # causes error for some reason
         use_raw=True,
         show=False,
+        color_map='Blues',
         save='_ensembl-{}.pdf'.format(out_file_base)
+    )
+    _ = sc.pl.dotplot(
+        adata,
+        marker_df_plt['ensembl_gene_id'].to_list(),
+        groupby='cluster',
+        dendrogram=True,
+        standard_scale='var',  # Scale color between 0 and 1
+        use_raw=True,
+        show=False,
+        color_map='Blues',
+        save='_ensembl-{}-standardized.pdf'.format(out_file_base)
+    )
+    _ = sc.pl.heatmap(
+        adata,
+        marker_df_plt['ensembl_gene_id'].to_list(),
+        groupby='cluster',
+        dendrogram=True,
+        use_raw=True,
+        show=False,
+        save='_ensembl-{}-standardized.pdf'.format(out_file_base)
+    )
+    _ = sc.pl.heatmap(
+        adata,
+        marker_df_plt['ensembl_gene_id'].to_list(),
+        groupby='cluster',
+        dendrogram=True,
+        standard_scale='var',  # Scale color between 0 and 1
+        use_raw=True,
+        show=False,
+        save='_ensembl-{}-standardized.pdf'.format(out_file_base)
     )
 
 
