8d2bedef2f6bf014755e143fb1ce463b1f117ca4
Leland Taylor <letaylor@users.noreply.github.com>
2020-04-24 12:30:47 +0100

    cleaned up resolution script

--- pipelines/0025-qc_cluster/bin/0057-scanpy_cluster_validate_resolution.py
+++ pipelines/0025-qc_cluster/bin/0057-scanpy_cluster_validate_resolution.py
@@ -2,7 +2,7 @@
 
 
 __author__ = 'Leland Taylor'
-__date__ = '2020-03-13'
+__date__ = '2020-04-24'
 __version__ = '0.0.1'
 
 import argparse
@@ -21,8 +21,7 @@ from sklearn.linear_model import LogisticRegression
 import joblib  # for numpy matrix, joblib faster than pickle
 
 
-# based on NaiveDE
-def create_colors(lr):
+def _create_colors(lr):
     n_cts = lr.classes_.shape[0]
     color_norm = colors.Normalize(vmin=-n_cts / 3, vmax=n_cts)
     ct_arr = np.arange(n_cts)
@@ -30,9 +29,10 @@ def create_colors(lr):
 
     return ct_colors
 
-# based on NaiveDE
+
 def plot_roc(y_prob, y_test, lr):
-    ct_colors = create_colors(lr)
+    """Plot ROC curve. Based off of NaiveDE library."""
+    ct_colors = _create_colors(lr)
 
     for i, cell_type in enumerate(lr.classes_):
         fpr, tpr, _ = metrics.roc_curve(y_test == cell_type, y_prob[:, i])
@@ -43,34 +43,54 @@ def plot_roc(y_prob, y_test, lr):
     plt.ylabel('TPR')
 
 
-# based on NaiveDE
 def logistic_model(
     X,
     cell_labels,
     sparsity=1.0,
     test_size=0.25,
     n_jobs=None,
-    verbose=True
+    verbose=True,
+    standarize=True,
+    with_mean=False
 ):
+    """Fit logistic regression model. Based off of NaiveDE."""
     # Standarize features - this is especially important for SAGA
-    # This scaler can also be applied to sparse CSR or CSC matrices by
+    # https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html
+    #
+    # Many elements used in the objective function of a learning algorithm
+    # (such as the RBF kernel of Support Vector Machines or the L1 and L2
+    # regularizers of linear models) assume that all features are centered
+    # around 0 and have variance in the same order. If a feature has a
+    # variance that is orders of magnitude larger that others, it might
+    # dominate the objective function and make the estimator unable to
+    # learn from other features correctly as expected.
+    #
+    # NOTE: This scaler can also be applied to sparse CSR or CSC matrices by
     # passing with_mean=False to avoid breaking the sparsity structure
     # of the data.
-    scaler = preprocessing.StandardScaler(
-        with_mean=False,
-        with_std=True
-    )
-    X_std = scaler.fit_transform(X)
-    # X_std = (X / X.std()).dropna(1)  # Does not handle sparse matrix
-    if verbose:
-        print('Scaled X')
-        print(type(X_std))
+    #
+    # Standarization is also recommended by authors of
+    # The Elements of Statistical Learning: Data Mining, Inference, and
+    # Prediction. 9780387216065
+    if standarize:
+        scaler = preprocessing.StandardScaler(
+            with_mean=with_mean,
+            with_std=True
+        )
+        X_std = scaler.fit_transform(X)
+        # X_std = (X / X.std()).dropna(1)  # Does not handle sparse matrix
+        if verbose:
+            print('Scaled X, with_mean={}'.format(with_mean))
+            print(type(X_std))
+    else:
+        X_std = X
 
     # Split the data into training and test data.
-    # NOTE: does it make sense to stratify sampling by cell_lables?
+    # NOTE: does it make sense to stratify splitting by cell_lables?
     X_train, X_test, y_train, y_test = model_selection.train_test_split(
         X_std,
         cell_labels,
+        # stratify=cell_labels,
         test_size=test_size
     )
     if verbose:
@@ -83,10 +103,18 @@ def logistic_model(
         print(type(X_train))
 
     # As noted in scanpy documentation:
+    # https://icb-scanpy.readthedocs-hosted.com/en/stable/api/scanpy.tl.rank_genes_groups.html
     # penalty='l1' to try to come up with a minimal set of genes that are good
     # predictors (sparse solution meaning few non-zero fitted coefficients).
     #
-    # SAGA is a good solver for large datasets and supports l1 penalty.
+    # L1 = Lasso Regression, L2 = Ridge Regression. Because Lasso shrinks the
+    # less important feature’s coefficient to zero (removing some feature
+    # altogether), it works well for feature selection when there are a
+    # large number of features.
+    # https://scikit-learn.org/stable/auto_examples/linear_model/plot_logistic_l1_l2_sparsity.html
+    #
+    # SAGA is a good solver for large datasets  both the number of samples
+    # and the number of features) and supports l1 penalty.
     # https://stackoverflow.com/questions/38640109/logistic-regression-python-solvers-defintions
     lr = LogisticRegression(
         penalty='l1',
@@ -97,30 +125,35 @@ def logistic_model(
     lr.fit(X_train, y_train)
     if verbose:
         print('Completed: LogisticRegression.')
+        print('Number of genes with > 0 coefficients:\t{}'.format(
+            (lr.coef_ > 0).sum(1)
+        ))
 
-    y_prob = lr.predict_proba(X_test)
+    # NOTE: P-value estimation
+    # https://scikit-learn.org/stable/modules/linear_model.html#logistic-regression
+    # It is possible to obtain the p-values and confidence intervals for
+    # coefficients in cases of regression without penalization. The
+    # statsmodels package <https://pypi.org/project/statsmodels/> natively
+    # supports this. Within sklearn, one could use bootstrapping instead
+    # as well.
+
+    # y_prob = lr.predict_proba(X_test)
+    y_prob = pd.DataFrame(
+        lr.predict_proba(X_test),
+        columns=lr.classes_
+    )
+    y_prob['cell_label_true'] = y_test
     if verbose:
         print('Completed: predict_proba.')
-        print(type(y_prob))
 
-    if verbose:
-        print((lr.coef_ > 0).sum(1))
-
-    lr_res = pd.DataFrame.from_records(
-        lr.coef_
-        # columns=X_std.columns
-    )
-
-    return y_prob, y_test, lr_res, lr
+    return lr, y_prob
 
 
 def main():
     """Run CLI."""
     parser = argparse.ArgumentParser(
         description="""
-            Read AnnData object and PCs file. Clusters the data. Saves an
-            AnnData object with clusters in the clusters slot, a clusters
-            file, and QC plots.
+            Fits logistic regression to predict labels in adata.obs["cluster"]'
             """
     )
 
@@ -142,9 +175,11 @@ def main():
         '-s', '--sparsity',
         action='store',
         dest='sparsity',
-        default=0.25,
+        default=0.1,
         type=float,
-        help='Sparsity.\
+        help='LogisticRegression sparsity or inverse of regularization\
+            strength; must be a positive float. Like in support vector\
+            machines, smaller values specify stronger regularization.\
             (default: %(default)s)'
     )
 
@@ -156,6 +191,16 @@ def main():
         type=float,
         help='Test size fraction.\
             (default: %(default)s)'
+    )
+
+    parser.add_argument(
+        '-ncells', '--number_cells',
+        action='store',
+        dest='ncells',
+        default=0,
+        type=int,
+        help='Downsample to this number of cells.\
+            (default: no downsampling)'
     )
 
     parser.add_argument(
@@ -181,11 +226,11 @@ def main():
     options = parser.parse_args()
 
     # Fixed settings.
-    # verbose = True
+    verbose = True
 
     # Scanpy settings
-    sc.settings.figdir = os.getcwd()  # figure output directory to match base.
-    sc.settings.n_jobs = options.ncpu  # number CPUs
+    # sc.settings.figdir = os.getcwd()  # figure output directory to match base
+    # sc.settings.n_jobs = options.ncpu  # number CPUs
     # sc.settings.max_memory = 500  # in Gb
     # sc.set_figure_params(dpi_save = 300)
 
@@ -201,12 +246,50 @@ def main():
     # clusters slot.
     adata = sc.read_h5ad(filename=options.h5)
 
-    # TODO: Optionally downsample cells
+    # Optionally downsample cells
+    if options.ncells > 0:
+        n_cells_start = adata.n_obs
+        if n_cells_start < options.ncells:
+            raise Exception('Fewer cells than ncells specified.')
+        # Downsample cells by fraction
+        # sc.pp.subsample(
+        #     adata,
+        #     fraction=0.2,
+        #     copy=False
+        # )
+        # Downasmple cells to a specific number.
+        sc.pp.subsample(
+            adata,
+            n_obs=options.ncells,
+            copy=False
+        )
+        print('Cell downsample applied: {} dropped, {} remain.'.format(
+            n_cells_start - adata.n_obs,
+            adata.n_obs
+        ))
 
     # Set X to raw counts
-    X = adata.layers['counts']
+    adata.X = adata.layers['counts']
     # Alternative: set X to log1p_cp10k
-    # X = adata.layers['log1p_cp10k']
+    # adata.X = adata.layers['log1p_cp10k']
+
+    # Ensure un-informative genes are filtered
+    # sc.pp.filter_genes(adata, min_cells=5)
+
+    # Subset to only highly variable genes
+    subset_to_hcg = False
+    if subset_to_hcg:
+        adata = adata[:, adata.var['highly_variable']]
+
+    # NOTE: We could use scanpy to stanardize the data, but just like sklearn
+    #       this will also change the matrix from Compressed Sparse Row format
+    #       to dense format.
+    # sc.pp.scale(
+    #     adata,
+    #     zero_center=True,
+    #     max_value=None,
+    #     copy=False
+    # )
 
     # NOTE: no need to make dense with sklearn... can keep spase
     # X = pd.DataFrame(X.toarray())
@@ -216,56 +299,72 @@ def main():
     # Inverse of regularization strength; must be a positive float.
     # Like in support vector machines, smaller values specify
     # stronger regularization.
-    #
-    # NOTE: for train / test split in logistic_model, this is done
-    # via sklearn.model_selection.train_test_split... it may make
-    # sense to set stratify to cell types.
-    # cell_labels.logistic_model(
-    test_prob, test_truth, lr_res, lr = logistic_model(
-        X,
+    lr, test_results = logistic_model(
+        adata.X,
         adata.obs['cluster'].values,
         sparsity=options.sparsity,
         test_size=options.test_size,
         n_jobs=options.ncpu,
-        verbose=True
-    )
-
-    # Save the lr_res dataframe.
-    lr_res.to_csv(
-        '{}-lr_res.tsv.gz'.format(out_file_base),
-        sep='\t',
-        index=True,
-        quoting=csv.QUOTE_NONNUMERIC,
-        na_rep='',
-        compression='gzip'
-    )
-
-    # Save the test results
-    test_results = {
-        'prob': test_prob,
-        'truth': test_truth
-    }
-    joblib.dump(
-        test_results,
-        '{}-test_results.joblib.gz'.format(out_file_base),
-        compress=('gzip', 5)
+        verbose=verbose
     )
 
     # Save the model
     joblib.dump(
         lr,
         '{}-lr.joblib.gz'.format(out_file_base),
-        compress=('gzip', 5)
+        compress=('gzip', 3)
     )
+    # Example of how to load a model.
+    # lr = joblib.load(
+    #     'CD5677E01F0E30D5-adata-normalized_pca-clustered-lr.joblib.gz'
+    # )
+    if verbose:
+        print('Completed: save lr.')
+
+    # Save the test results
+    test_results.to_csv(
+        '{}-test_results.tsv.gz'.format(out_file_base),
+        sep='\t',
+        index=True,
+        index_label='cell_label',
+        quoting=csv.QUOTE_NONNUMERIC,
+        na_rep='',
+        compression='gzip'
+    )
+    if verbose:
+        print('Completed: save test_results.')
 
     # Save the ROC of the test and truth.
-    fig, ax = plt.subplot(2, 1, 2)
-    plot_roc(test_prob, test_truth, lr)
+    fig = plt.figure()
+    cell_label_true = test_results.pop('cell_label_true')
+    plot_roc(test_results.values, cell_label_true.values, lr)
     fig.savefig(
         '{}-roc.pdf'.format(out_file_base),
         dpi=300,
         bbox_inches='tight'
     )
+    if verbose:
+        print('Completed: save ROC plot.')
+
+    # Make a dataframe of the coefficients.
+    # Rows = cell type label and columns = genes tested
+    lr_res = pd.DataFrame.from_records(
+        lr.coef_,
+        index=lr.classes_,
+        columns=adata.var.index
+    )
+    # Save the lr_res dataframe.
+    lr_res.to_csv(
+        '{}-lr_coef.tsv.gz'.format(out_file_base),
+        sep='\t',
+        index=True,
+        index_label='cell_label',
+        quoting=csv.QUOTE_NONNUMERIC,
+        na_rep='',
+        compression='gzip'
+    )
+    if verbose:
+        print('Completed: save lr_res.')
 
 
 if __name__ == '__main__':
