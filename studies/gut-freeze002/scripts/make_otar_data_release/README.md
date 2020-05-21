446bd90be82e0f1db5a3a9085c5423effcdb3d4b
Leland Taylor <letaylor@users.noreply.github.com>
2020-05-21 07:57:30 +0100

    added scripts to make OTAR data release

---
+++ studies/gut-freeze002/scripts/make_otar_data_release/README.md
@@ -0,0 +1,27 @@
+
+# DETAILS
+
+* Project number: OTAR2-057
+* Team Email: otar2057@opentargets.org
+* Data freeze: v002
+
+# SUMMARY
+
+Single cell RNA-seq (scRNA-seq) of terminal ileum biopsies from 25 Crohn's disease (CD) and 8 healthy participants. This release includes: (1) 3' 10x scRNA-seq data processed with CellRanger v3.0.2 (genome reference = GRCh38) by Sanger DNA pipelines, (2) phenotype information on particpants, and (3) sample metadata.
+
+Note that we include two CellRanger output datasets, labelled "filtered_feature_bc_matrix" and "raw_feature_bc_matrix". "Filtered" refers to data where empty droplets have been removed as part of standard CellRanger analysis. For the "raw_feature" data, empty droplets have not been removed. 
+
+
+# PROJECT OVERVIEW AND GOALS
+
+The goal of this project to integrate data across all available and relevant sources in order to answer the following questions:
+
+1) What genes have the highest probability of mediating GWAS signals and, therefore, to play a causal role in IBD?
+
+2) What are the broader phenotypic consequences of perturbing these genes?
+
+This effort will inform drug development efforts in IBD, as we will identify potential novel targets (list of causal genes), on-target safety concerns (PheWAS), additional indications (PheWAS), and early estimates of efficacy and therapeutic windows (dose-response modeling). Through detailed review of individual genes as potential drug targets, this work will also serve as an application-focused pilot for the Open Targets genetics portal. Examples pursued in this proposal will provide use cases to help guide the development of work flows, tools, and visualization strategies.
+
+It is often difficult to draw insight into drug targets from GWAS studies because many of the signals lie outside of genes, in sequence that is presumed regulatory. Without knowledge of which genes are regulated by these non-coding variants, one cannot infer drug targets or uncover new disease insights. This is because the ability of GWAS to pinpoint IBD drug targets hinges on our capacity to correlate protein function with IBD risk, using genetic variants as a causal anchor. Thus, if we want to translate our significant IBD locus discovery efforts into biological understanding and drug targets, it is vital we create definitive genetic maps of gene regulation in disease relevant cells.
+
+While a lot of effort has been put into identifying eQTLs in the peripheral immune system, there are currently no cell-type specific eQTLs maps for gut tissue. This could explain why only around 15% of IBD loci currently colocalise with a known eQTL. Thankfully, gut tissue is readily accessible through routine clinical care. This, combined with recent progress in cellular and genomic technologies, presents a unique opportunity for deriving novel genetically validated IBD drug targets. In this project we will perform single-cell RNA sequencing (scRNAseq) of healthy terminal ileum biopsies from 350 patients undergoing screening for colorectal cancer to create the definitive TI cellular map. We will genotype these individuals, identify genetic variants associated with gene-expression (eQTLs), and combine these with existing IBD GWAS data to identify genes, cell types and pathways causally underpinning the disease.
---
+++ studies/gut-freeze002/scripts/make_otar_data_release/make_otar_release_data.R
@@ -0,0 +1,189 @@
+#!/usr/bin/env Rscript
+
+# disable strings as factors, but re-enable upon exit
+old <- options(stringsAsFactors = FALSE)
+on.exit(options(old), add = TRUE)
+
+suppressPackageStartupMessages(library("optparse"))
+suppressPackageStartupMessages(library("data.table"))
+suppressPackageStartupMessages(library("Seurat"))
+
+
+#' Command line interface wrapper
+#'
+#' @importFrom optparse make_option
+#' @importFrom optparse OptionParser
+#' @importFrom optparse parse_args
+#' @importFrom data.table fread
+#' @importFrom Seurat Read10X
+#' @importFrom Seurat CreateSeuratObject
+command_line_interface <- function() {
+    optionList <- list(
+        optparse::make_option(c("--file_paths_10x"),
+            type = "character",
+            help = paste0(
+                "Input file_paths_10x.tsv (all samples in the data freeze)."
+            )
+        ),
+
+        optparse::make_option(c("--cellranger_irods_locations"),
+            type = "character",
+            help = paste0(
+              "Locations of each file in irods (csv)."
+            )
+        ),
+
+        optparse::make_option(c("--metadata"),
+            type = "character",
+            help = paste0(
+              "Metadata file."
+            )
+        ),
+
+        optparse::make_option(c("--metadata_dict"),
+            type = "character",
+            help = paste0(
+              "Metadata definitions."
+            )
+        )
+    )
+
+    parser <- optparse::OptionParser(
+        usage = "%prog",
+        option_list = optionList,
+        description = paste0(
+            "Makes data for OTAR partner sharing."
+        )
+    )
+
+    # A hack to fix a bug in optparse that won't let you use positional args
+    # if you also have non-boolean optional args:
+    getOptionStrings <- function(parserObj) {
+        optionStrings <- character()
+        for (item in parserObj@options) {
+            optionStrings <- append(optionStrings,
+                                    c(item@short_flag, item@long_flag))
+        }
+        optionStrings
+    }
+    optStrings <- getOptionStrings(parser)
+    arguments <- optparse::parse_args(parser, positional_arguments = TRUE)
+
+    # read in the parameters
+    param <- list()
+    for (i in names(arguments$options)) {
+        param[[i]] <- arguments$options[[i]]
+    }
+
+    # Read in the sample sheet
+    df_samples <- read.csv(param[["file_paths_10x"]], sep = "\t")
+
+    # Read in cellranger_irods_locations
+    df_cellranger <- read.csv(
+        param[["cellranger_irods_locations"]],
+        sep = ","
+    )
+    rownames(df_cellranger) <- df_cellranger$sanger_sample_id
+
+    # Make output file with irods data location of the files to release.
+    df_cellranger_out <- df_cellranger[df_samples$experiment_id, ]
+    file_or_dir_endings_irods <- c(
+        "cellranger_filtered" = "filtered_feature_bc_matrix/",
+        "cellranger_unfiltered" = "raw_feature_bc_matrix/",
+        "bam_file" = "possorted_genome_bam.bam",
+        "bam_index" = "possorted_genome_bam.bai"
+
+    )
+    for (i in names(file_or_dir_endings_irods)) {
+        df_cellranger_out[[i]] <- paste0(
+            df_cellranger_out$cellranger_irods_path,
+            "/",
+            file_or_dir_endings_irods[[i]]
+        )
+    }
+    df_cellranger_out$cellranger_irods_path <- NULL
+    write.table(
+        df_cellranger_out,
+        "data_release-irods_locations.tsv",
+        row.names = FALSE,
+        col.names = TRUE,
+        quote = FALSE,
+        sep = "\t",
+        na = ""
+    )
+
+
+
+    # Read in the sample metadata
+    df_metadata <- read.csv(
+        param[["metadata"]],
+        sep = "\t"
+    )
+    rownames(df_metadata) <- df_metadata$sanger_sample_id
+
+    # Read in the sample metatadictionary
+    df_metadata_dict <- read.csv(
+        param[["metadata_dict"]],
+        sep = "\t"
+    )
+
+    # Subset the sample metadata
+    # EDIT: instead of subsetting here, just keep the
+    # cols_sample_metadata <- c(
+    #     "date_of_sample",
+    #     "date_of_plate_submission",
+    #     "patient_id",
+    #     "sanger_sample_id",
+    #     "sex",
+    #     "age",
+    #     "biopsy_type",
+    #     "disease_status",
+    #     "inflammation_status",
+    #     "cd_type",
+    #     "time_to_chromium_processing",
+    #     "total_reads"
+    #     # smoking_status
+    #     # experimentalist
+    #     # epithelial_immune_ratio
+    #     # protocol
+    #     # collagenase
+    #     # inhibitor
+    #     # DNase
+    #     # ack_lysis_buffer
+    #     # frozen_processed
+    #     # chip_well_position
+    #     # enzyme_lot
+    #     # bead_version
+    #     # bead_lot
+    #     # chip_version
+    #     # chip_lot
+    #     # id_run
+    #     # lane
+    #     # library_id
+    #     # library_type
+    #     # study
+    #     # study_id
+    # )
+    cols_sample_metadata <- df_metadata_dict$column_name
+    df_metadata <- df_metadata[df_samples$experiment_id, cols_sample_metadata]
+    write.table(
+        df_metadata,
+        "data_release-metadata.tsv",
+        row.names = FALSE,
+        col.names = TRUE,
+        quote = TRUE,
+        sep = "\t",
+        na = ""
+    )
+}
+
+
+main <- function() {
+    command_line_interface()
+}
+
+
+# like python if __name__ == '__main__'
+if (sys.nframe() == 0) {
+    main()
+}
