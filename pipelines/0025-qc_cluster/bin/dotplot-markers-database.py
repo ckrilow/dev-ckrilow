0917b38cc2e5b1999938435982951d4ed776ee2b
Monika Krzak <monika.sonia.krzak@gmail.com>
2020-05-29 16:34:07 +0100

    corrected dotplot

--- pipelines/0025-qc_cluster/bin/dotplot-markers-database.py
+++ pipelines/0025-qc_cluster/bin/dotplot-markers-database.py
@@ -7,10 +7,8 @@ __version__ = '0.0.1'
 
 import argparse
 import scanpy as sc
-import plotnine as plt9
 import pandas as pd
 import numpy as np
-
 
 
 def main():
@@ -42,25 +40,31 @@ def main():
 
     adata = sc.read_h5ad(filename=options.h5)
 
-    df=pd.read_table(options.markers_database)
-    
-    cell_type=np.unique(df['cell_type'])
+    df = pd.read_table(options.markers_database)
+
+    if 'p_value_adj' in df.columns:
+        df = df[['cell_type', 'hgnc_symbol', 'p_value_adj']]
+        df = df.sort_values(by=['cell_type', 'p_value_adj'],
+                            ascending=[True, True])
+    else:
+        df = df[['cell_type', 'hgnc_symbol']]
+
+    cell_type = np.unique(df['cell_type'])
     for i in cell_type:
         marker_genes = df.loc[df['cell_type'] == i]['hgnc_symbol']
-        marker_genes_found = adata.var['gene_symbols'][adata.var['gene_symbols'].isin(marker_genes)]
-    
-        if marker_genes_found.size>100: 
-            marker_genes_found=marker_genes_found[0:100]
+        marker_genes_found = adata.var['gene_symbols'][
+            adata.var['gene_symbols'].isin(marker_genes)
+        ]
 
-        plt = sc.pl.dotplot(adata, marker_genes_found,
-                        groupby='leiden', 
-                        gene_symbols='gene_symbols',
-                        dendrogram=True,
-                        save='scanpy_dotplot{}.png'.format(i))
-    
-    
-    
-    
+        if marker_genes_found.size > 100:
+            marker_genes_found = marker_genes_found[0:100]
+
+        sc.pl.dotplot(
+                        adata, marker_genes_found, groupby='leiden',
+                        gene_symbols='gene_symbols', dendrogram=True,
+                        save='scanpy_dotplot{}.png'.format(i)
+                        )
+                        
 
 if __name__ == '__main__':
     main()
