5f1cf1c3a4967dc303ea3eaf61b4863af78bc7f7
Monika Krzak <monika.sonia.krzak@gmail.com>
2020-06-02 16:50:22 +0100

    dotplot last changes

--- studies/gut-freeze002/scripts/dotplot-markers-database.py
+++ studies/gut-freeze002/scripts/dotplot-markers-database.py
@@ -12,7 +12,6 @@ import numpy as np
 import os
 
 
-
 def main():
     """Run CLI."""
     parser = argparse.ArgumentParser(
@@ -42,49 +41,40 @@ def main():
 
     adata = sc.read_h5ad(filename=options.h5)
 
-    markers=pd.read_table(options.markers_database, sep='\t', header=None)[0]
+    markers = pd.read_table(options.markers_database, sep='\t', header=None)[0]
 
-    
     for i in markers:
-    
-        base=os.path.basename(os.path.dirname(i))
+        base = os.path.basename(os.path.dirname(i))
         df = pd.read_table(i).copy()
 
-        if 'p_value_adj' in df.columns: 
-            df = df[['cell_type','hgnc_symbol', 'p_value_adj']].copy()
-            df = df.sort_values(by=['cell_type', 'p_value_adj'], ascending=[True, True]).copy()
+        if 'p_value_adj' in df.columns:
+            df = df[['cell_type', 'hgnc_symbol', 'p_value_adj']].copy()
+            df = df.sort_values(by=['cell_type', 'p_value_adj'],
+                                ascending=[True, True]).copy()
         else:
-            df = df[['cell_type','hgnc_symbol']].copy()
-      
-        
-        cell_type=np.unique(df['cell_type']).copy()
-    
-   
+            df = df[['cell_type', 'hgnc_symbol']].copy()
+        cell_type = np.unique(df['cell_type']).copy()
         for j in cell_type:
-                
             marker_genes = df.loc[df['cell_type'] == j]['hgnc_symbol'].copy()
-            marker_genes_found = adata.var['gene_symbols'][adata.var['gene_symbols'].isin(marker_genes)].copy()
-        
-    
-            if marker_genes_found.size>100: 
+            marker_genes_found = adata.var['gene_symbols'][
+                    adata.var['gene_symbols'].isin(marker_genes)].copy()
+            if marker_genes_found.size > 100:
                 marker_genes_found = marker_genes_found[0:100]
 
-            
             j_out = j.replace(' ', '_').replace('.', '_').replace('/', '-')
 
-            try:   
-                sc.pl.dotplot(adata, marker_genes_found,
-                        groupby='leiden', 
-                        gene_symbols='gene_symbols',
-                        dendrogram=True,
-                        show=False,
-                        save=base + ('-{}.png'.format(j_out)))
+            try:
+                sc.pl.dotplot(
+                                adata, marker_genes_found,
+                                groupby='leiden',
+                                gene_symbols='gene_symbols',
+                                dendrogram=True,
+                                show=False,
+                                save=base + ('-{}.png'.format(j_out))
+                                )
             except:
                 pass
-        
-        
-    
-    
+
 
 if __name__ == '__main__':
     main()
