62b1c7ca66a972dacd91715dd600df5cc5d0440d
Henry Taylor <henryjtaylor@users.noreply.github.com>
2020-06-27 09:13:36 -0400

    fix ipsc2beta configs

--- studies/chen-t1d/conditions_collins/run_pipeline.sh
+++ studies/chen-t1d/conditions_collins/run_pipeline.sh
@@ -11,7 +11,7 @@ rm trace.txt.*;
 export REPO_MODULE="/cluster/ifs/projects/collins/taylorhj/projects/sc_nextflow/pipelines/0025-qc_cluster"
 export STUDY_DIR="/cluster/ifs/projects/collins/taylorhj/projects/sc_nextflow/studies/chen-t1d/conditions_collins"
 #export OUTPUT_DIR="${STUDY_DIR}/results/min_parameter_sweep"
-export OUTPUT_DIR="${STUDY_DIR}/results/nf-sample_qc=mt80_singlet-parameter_sweep_v001_harmony"
+export OUTPUT_DIR="${STUDY_DIR}/results/nf-sample_qc=parker_singlet-parameter_sweep_v001_harmony"
 
 # Nextflow settings
 export JAVA_HOME="/cluster/ifs/projects/collins/taylorhj/bin/jre1.8.0_251"
@@ -41,7 +41,7 @@ export QT_QPA_PLATFORM='offscreen'
      -profile "sge_nih_trek" \
      --file_paths_10x "${STUDY_DIR}/file_paths_10x.tsv" \
      --file_metadata "${STUDY_DIR}/conditions_clinical_cr_metrics.tsv" \
-     --file_sample_qc "${STUDY_DIR}/params-sample_qc-doublets_mito0pt80.yml" \
+     --file_sample_qc "${STUDY_DIR}/params-sample_qc-doublets_parker_cutoffs.yml" \
      --genes_exclude_hvg "${STUDY_DIR}/../data-variable_gene_filter_ribo_mt.tsv" \
      --genes_score "${STUDY_DIR}/../data-gene_scores.tsv" \
      --output_dir "${OUTPUT_DIR}" \
