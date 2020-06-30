357e99d334175430338d8e1dbcbc6574582a0d5b
Henry Taylor <henryjtaylor@users.noreply.github.com>
2020-06-29 21:55:33 -0400

    run harmony sweep on ipsc2beta data

--- studies/ipsc2beta/E11/run_pipeline.sh
+++ studies/ipsc2beta/E11/run_pipeline.sh
@@ -10,7 +10,7 @@ rm trace.txt.*;
 
 export REPO_MODULE="/cluster/ifs/projects/collins/taylorhj/projects/sc_nextflow/pipelines/0025-qc_cluster"
 export STUDY_DIR="/cluster/ifs/projects/collins/taylorhj/projects/sc_nextflow/studies/ipsc2beta/E11"
-export OUTPUT_DIR="${STUDY_DIR}/results/min_parameter_sweep"
+export OUTPUT_DIR="${STUDY_DIR}/results/harmony_sweep_mt20"
 #export OUTPUT_DIR="${STUDY_DIR}/results/nf-sample_qc=mt80_singlet-parameter_sweep_v001_harmony"
 
 # Nextflow settings
@@ -41,11 +41,11 @@ export QT_QPA_PLATFORM='offscreen'
      -profile "sge_nih_trek" \
      --file_paths_10x "${STUDY_DIR}/file_paths_10x.tsv" \
      --file_metadata "${STUDY_DIR}/sample_metadata_cr_metrics.tsv" \
-     --file_sample_qc "${STUDY_DIR}/params-sample_qc-no_filters.yml" \
+     --file_sample_qc "${STUDY_DIR}/params-sample_qc-doublets_mito0pt20.yml" \
      --genes_exclude_hvg "${STUDY_DIR}/../data-variable_gene_filter_ribo_mt.tsv" \
      --genes_score "${STUDY_DIR}/../data-gene_scores.tsv" \
      --output_dir "${OUTPUT_DIR}" \
-     -params-file "${STUDY_DIR}/params-analysis-parameter_sweep_min_params.yml" \
+     -params-file "${STUDY_DIR}/params-analysis-parameter_sweep_v001_harmony.yml" \
      --run_multiplet \
      -with-report \
      -with-trace \
