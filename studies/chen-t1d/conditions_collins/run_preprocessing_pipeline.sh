b1a1597eda9982c04a1049daf572110e10caf728
Henry Taylor <henryjtaylor@users.noreply.github.com>
2020-07-06 11:23:55 -0400

    update settings for cellbender test

--- studies/chen-t1d/conditions_collins/run_preprocessing_pipeline.sh
+++ studies/chen-t1d/conditions_collins/run_preprocessing_pipeline.sh
@@ -10,8 +10,7 @@ rm trace.txt.*;
 
 export REPO_MODULE="/cluster/ifs/projects/collins/taylorhj/projects/sc_nextflow/pipelines/0015-preprocessing"
 export STUDY_DIR="/cluster/ifs/projects/collins/taylorhj/projects/sc_nextflow/studies/chen-t1d/conditions_collins"
-#export OUTPUT_DIR="${STUDY_DIR}/results/min_parameter_sweep"
-export OUTPUT_DIR="${STUDY_DIR}/results/nf-preprocessing=parker_singlet"
+export OUTPUT_DIR="${STUDY_DIR}/results/nf-preprocessing=parker_test"
 
 # Nextflow settings
 export JAVA_HOME="/cluster/ifs/projects/collins/taylorhj/bin/jre1.8.0_251"
@@ -40,10 +39,6 @@ export QT_QPA_PLATFORM='offscreen'
     "${REPO_MODULE}/main.nf" \
      -profile "sge_nih_trek" \
      --file_paths_10x "${STUDY_DIR}/file_paths_10x_unfiltered.tsv" \
-     --file_metadata "${STUDY_DIR}/conditions_clinical_cr_metrics.tsv" \
-     --file_sample_qc "${STUDY_DIR}/preprocessing_configs/params-sample_qc-doublets_parker_cutoffs.yml" \
-     --genes_exclude_hvg "${STUDY_DIR}/../data-variable_gene_filter_ribo_mt.tsv" \
-     --genes_score "${STUDY_DIR}/../data-gene_scores.tsv" \
      --output_dir "${OUTPUT_DIR}" \
      -params-file "${STUDY_DIR}/preprocessing_configs/params.yml" \
      --run_multiplet \
