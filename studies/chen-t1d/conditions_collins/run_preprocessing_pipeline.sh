50ef9bd150410ad4531ccbcbb69ec82df0be5a88
Henry Taylor <henryjtaylor@users.noreply.github.com>
2020-07-21 10:57:05 -0400

    add config to sge-trek, setup qc pipeline for cell bender

--- studies/chen-t1d/conditions_collins/run_preprocessing_pipeline.sh
+++ studies/chen-t1d/conditions_collins/run_preprocessing_pipeline.sh
@@ -10,8 +10,8 @@ rm trace.txt.*;
 
 export REPO_MODULE="/cluster/ifs/projects/collins/taylorhj/projects/sc_nextflow/pipelines/0015-preprocessing"
 export STUDY_DIR="/cluster/ifs/projects/collins/taylorhj/projects/sc_nextflow/studies/chen-t1d/conditions_collins"
-#export OUTPUT_DIR="${STUDY_DIR}/results/nf-preprocessing=parker"
-export OUTPUT_DIR="${STUDY_DIR}/results/nf-preprocessing=parker_test"
+export OUTPUT_DIR="${STUDY_DIR}/results/nf-preprocessing=parker"
+#export OUTPUT_DIR="${STUDY_DIR}/results/nf-preprocessing=parker_test"
 
 # Nextflow settings
 export JAVA_HOME="/cluster/ifs/projects/collins/taylorhj/bin/jre1.8.0_251"
@@ -39,9 +39,9 @@ export QT_QPA_PLATFORM='offscreen'
 /home/taylorhj/miniconda3/envs/nextflow20/bin/nextflow run \
     "${REPO_MODULE}/main.nf" \
      -profile "sge_nih_trek" \
-     --file_paths_10x "${STUDY_DIR}/file_paths_10x_unfiltered_two_sample.tsv" \
+     --file_paths_10x "${STUDY_DIR}/file_paths_10x_unfiltered.tsv" \
      --output_dir "${OUTPUT_DIR}" \
-     -params-file "${STUDY_DIR}/preprocessing_configs/params_test.yml" \
+     -params-file "${STUDY_DIR}/preprocessing_configs/params.yml" \
      --run_multiplet \
      -with-report \
      -with-trace \
--- studies/chen-t1d/conditions_collins/run_qc_cluster_pipeline.sh
+++ studies/chen-t1d/conditions_collins/run_qc_cluster_pipeline.sh
@@ -11,7 +11,7 @@ rm trace.txt.*;
 export REPO_MODULE="/cluster/ifs/projects/collins/taylorhj/projects/sc_nextflow/pipelines/0025-qc_cluster"
 export STUDY_DIR="/cluster/ifs/projects/collins/taylorhj/projects/sc_nextflow/studies/chen-t1d/conditions_collins"
 #export OUTPUT_DIR="${STUDY_DIR}/results/min_parameter_sweep"
-export OUTPUT_DIR="${STUDY_DIR}/results/nf-sample_qc=parker_singlet-parameter_sweep_v001_harmony"
+export OUTPUT_DIR="${STUDY_DIR}/results/nf-sample_qc=cellbender_parker_singlet-parameter_sweep_v001_harmony"
 
 # Nextflow settings
 export JAVA_HOME="/cluster/ifs/projects/collins/taylorhj/bin/jre1.8.0_251"
@@ -39,7 +39,7 @@ export QT_QPA_PLATFORM='offscreen'
 /home/taylorhj/miniconda3/envs/nextflow20/bin/nextflow run \
     "${REPO_MODULE}/main.nf" \
      -profile "sge_nih_trek" \
-     --file_paths_10x "${STUDY_DIR}/file_paths_10x.tsv" \
+     --file_paths_10x "${STUDY_DIR}/epoch_1000_lr_1.0E-4.tsv" \
      --file_metadata "${STUDY_DIR}/conditions_clinical_cr_metrics.tsv" \
      --file_sample_qc "${STUDY_DIR}/qc_cluster_configs/params-sample_qc-doublets_parker_cutoffs.yml" \
      --genes_exclude_hvg "${STUDY_DIR}/../data-variable_gene_filter_ribo_mt.tsv" \
